74e192e9879c9a25315ad2ab186236fff8a622db
Evaldo Cardoso <Evaldopkscardoso@gmail.com>
2019-12-23 15:15:47 -0400

    adding category model

--- finansys/src/app/in-memory-database.ts
+++ finansys/src/app/in-memory-database.ts
@@ -1,14 +1,17 @@
 import {InMemoryDbService } from 'angular-in-memory-web-api';
+import { Category } from './pages/categories/shared/category.model';
 
 export class InMemoryDatabase implements InMemoryDbService{
     createDb() {
-        const categories = [
+        const categories: Category[] = [
             {id: 1, name: 'moradia', description: 'Pagamentos de Contas da Casa'},
             {id: 2, name: 'Saúde', description: 'Planos de Saúde e Remédios'},
             {id: 3, name: 'Lazer', description: 'Cinema, parques, paraia,etc'},
             {id: 4, name: 'Salário', description: 'Recebimento de salário'},
             {id: 5, name: 'Freelas', description: 'Trabalhos como freelancer'}
         ];
+
+        return(categories);
     }
 
 }
